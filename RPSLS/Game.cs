969ef2fef7bf3afed76eb0d197321af963bd9acd
awlkr <dreawlkr523@gmail.com>
2023-09-18 20:10:51 -0500

    Rules explained, player amount selected

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -21,12 +21,28 @@ namespace RPSLS
         //Member Methods (CAN DO)
         public void WelcomeMessage()
         {
-            Console.WriteLine("Welcome to RPSLS! Here are the rules:\n");
+            Console.WriteLine("Welcome to RPSLS! Firstly, you will select the number of people that will be playing. Next, you duel! Here are the rules to the game:");
+
+            Console.WriteLine("Rock crushes Scissors" +
+                "Scissors cuts Paper" +
+                "Paper covers Rock" +
+                "Rock crushes Lizard" +
+                "Lizard poisons Spock" +
+                "Spock smashes Scissors" +
+                "Scissors decapitates Lizard" +
+                "Lizard eats Paper" +
+                "Paper disproves Spock" +
+                "Spock vaporizes Rock");
+
+            Console.WriteLine("Game will be best out of 3 rounds!" +
+                "Good luck!");
+
         }
 
         public int ChooseNumberOfHumanPlayers()
         {
-            return 0;
+            Console.WriteLine("How many players will be deuling today?");
+            string numberOfHumanPlayers = Console.ReadLine();
         }
 
         public void CreatePlayerObjects(int numberOfHumanPlayers)
